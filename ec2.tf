5ac0af7d927fd9f1786d624992bb868f5df0184c
jagadisdevops <134026862+jagadisdevops@users.noreply.github.com>
2023-08-08 11:54:58 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = "ami-02358d9f5245918a3"
+  ami           = "ami-064bd2d44a1d6c097"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "practice-terraform"
   }
 }
